6ea7a0f24492ec71a9f70c0ce03403c10dfd9d01
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-10-12 15:00:02 +0100

    Updating React components - updating Form Navbar,Login,Register. Creating hook to handle profile requests for different pages.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,12 @@
 import Form from "../components/Form"
 
 function Register() {
-    return <Form route="/api/user/register/" method="register"/>
+    const handleSuccessfulAuth = () => {
+        // Your logic to fetch user info or update state
+        console.log("User successfully Created, fetching user info...");
+        fetchUserInfo(); // Assuming this function is defined in the parent or the same component
+    };
+    return <Form route="/api/user/register/" method="register" onSuccessfulAuth={handleSuccessfulAuth} />
     
 }
 
