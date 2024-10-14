0bc23a8248a9a51707b009de2cc4077f9d66fdf8
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-10-14 19:05:19 +0100

    Updating build settings for react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true, // This ensures that React Router works correctly
+    proxy: {
+      '/api': 'http://127.0.0.1:8000',  // Redirects any requests to /api to Django backend
+    }
+  }
 })
