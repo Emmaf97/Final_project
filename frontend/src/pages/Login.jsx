9983d38a6459ef9611b15b914707c3c0b87a4b17
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-10-14 11:42:29 +0100

    Updating footer style and cleaning contact & login of logs

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,6 @@ import Form from "../components/Form"
 function Login() {
     const handleSuccessfulAuth = () => {
         // Your logic to fetch user info or update state
-        console.log("User successfully logged in, fetching user info...");
         fetchUserInfo(); // Assuming this function is defined in the parent or the same component
     };
     return <Form route="/api/token/" method="login" onSuccessfulAuth={handleSuccessfulAuth}  />;
