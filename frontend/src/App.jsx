29fdc45a26a53c48400e3475879e67de2f4c5637
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-10-14 15:38:18 +0100

    Updating app.jsx error during deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
             {/* Other routes */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/map" element={<SimpleMap />} />
+            {/* <Route path="/map" element={<SimpleMap />} /> */}
             <Route path="/home" element={<Home />} />
             <Route path="/myths" element={<Myths />} />
             <Route path="/folklore" element={<Folklore />} />
